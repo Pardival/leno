7a30b50859363f672475a676c6049c9f2a0b8f4f
Kevin Dufour <kevin.dufourpro@gmail.com>
2026-07-07 15:24:42 +1000

    PAR-7 ifra: configuration of Wishper and llm

--- services/main.py
+++ services/main.py
@@ -10,11 +10,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 app = FastAPI()
-client = OpenAI(api_key = os.getenv("OPENAI_API"))
+open_ai = OpenAI(api_key = os.getenv("OPENAI_API"))
 
 @app.get("/")
 async def read_root():
-    response = client.responses.create(
+    response = open_ai.responses.create(
     model="gpt-5.4-nano",
     input="Write a one-sentence bedtime story about a unicorn.")
     print(response.output_text)
